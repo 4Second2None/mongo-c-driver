979195d6257213ddad024aef0d48968635a8ef07
cwlalyy <cwlalx@163.com>
2014-12-08 08:27:11 +0800

    Update gridfs.c
    store buffers of gridfs by objectId

--- src/gridfs.c
+++ src/gridfs.c
@@ -208,6 +208,38 @@ MONGO_EXPORT int gridfs_store_buffer( gridfs *gfs, const char *data,
     return gridfs_insert_file( gfs, remotename, id, length, contenttype );
 }
 
+MONGO_EXPORT int gridfs_store_buffer2( gridfs *gfs, const char *data,
+                          gridfs_offset length, const char *remotename,
+                          const char *contenttype, const char * objid ) {
+
+    char const *end = data + length;
+    const char *data_ptr = data;
+    bson_oid_t id;
+    int chunkNumber = 0;
+    int chunkLen;
+    bson *oChunk;
+	bson_oid_from_string( &id, objid );
+    /* Large files Assertion */
+    /* assert( length <= 0xffffffff ); */
+
+    /* Generate and append an oid*/
+    //bson_oid_gen( &id );
+
+    /* Insert the file's data chunk by chunk */
+    while ( data_ptr < end ) {
+        chunkLen = DEFAULT_CHUNK_SIZE < ( unsigned int )( end - data_ptr ) ?
+                   DEFAULT_CHUNK_SIZE : ( unsigned int )( end - data_ptr );
+        oChunk = chunk_new( id, chunkNumber, data_ptr, chunkLen );
+        mongo_insert( gfs->client, gfs->chunks_ns, oChunk, NULL );
+        chunk_free( oChunk );
+        chunkNumber++;
+        data_ptr += chunkLen;
+    }
+
+    /* Inserts file's metadata */
+    return gridfs_insert_file( gfs, remotename, id, length, contenttype );
+}
+
 MONGO_EXPORT void gridfile_writer_init( gridfile *gfile, gridfs *gfs,
                            const char *remote_name, const char *content_type ) {
     gfile->gfs = gfs;
